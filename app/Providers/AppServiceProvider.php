45e7ffccae790b59f01f223e467b35b68bd25623
Armil Purificacion <armilwebdev@gmail.com>
2019-10-07 05:55:07 +0800

    deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,36 +11,22 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-    public function boot(UrlGenerator $url)
-    {
-        if(env('REDIRECT_HTTPS')) {
-            $url->formatScheme('https');
-        }
-    }
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        if(env('REDIRECT_HTTPS')) {
-            $this->app['request']->server->set('HTTPS', true);
-        }
-    }
-    
+     public function boot(UrlGenerator $url)
+     {
+         if(env('REDIRECT_HTTPS')) {
+             $url->formatScheme('https');
+         }
+     }
+ 
+     /**
+      * Register any application services.
+      *
+      * @return void
+      */
+     public function register()
+     {
+         if(env('REDIRECT_HTTPS')) {
+             $this->app['request']->server->set('HTTPS', true);
+         }
+     }
 }
